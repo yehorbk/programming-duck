b8990e046d6c88eda829121c92ae459f042ae4c0
Yehor Bublyk <yehor.bk@gmail.com>
2021-05-09 01:27:31 +0300

    typo fixed

--- src/models/client-message.js
+++ src/models/client-message.js
@@ -1,5 +1,6 @@
 'use strict';
 
 module.exports = {
-  GREETING: "Hey! I'm a Programming Duck 🦆\nJust tell me your problem and I will help you to fix it! 😉",
+  GREETING:
+    "Hey! I'm a Programming Duck 🦆\nJust tell me your problem and I will help you to fix it! 😉",
 };
